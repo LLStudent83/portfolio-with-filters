c13b854a7289ce90833709496438769ed74aaf93
Dmitry Prigulnov <prigulnov@mail.ru>
2022-04-09 12:55:41 +0500

    переназначил key

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,6 +1,6 @@
-/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
+import shortId from 'shortid';
 import PropTypes from 'prop-types';
 import Project from './Project';
 import './projectList.css';
@@ -8,8 +8,8 @@ import './projectList.css';
 function ProjectList({ stateProjects }) {
   return (
     <section className="projectList"> {
-        stateProjects.map((project, i) => (
-          <Project img={project.img} key={i} />
+        stateProjects.map((project) => (
+          <Project img={project.img} key={shortId.generate()} />
         ))
             }
     </section>
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,18 +1,18 @@
-/* eslint-disable react/no-array-index-key */
 import PropTypes, { func } from 'prop-types';
 import React from 'react';
+import shortId from 'shortid';
 import ToolbarButton from './ToolbarButton';
 
 function Toolbar({ filters, selected, onSelectFilter }) {
   return (
     <header>
       <ul className="toolbar__list">
-        {filters.map((filter, i) => (
+        {filters.map((filter) => (
           <ToolbarButton
             selected={selected}
             onClick={onSelectFilter}
             name={filter}
-            key={i}
+            key={shortId.generate()}
           />
         ))}
       </ul>
